0d4e506b6c5a1869e8901f4759e167ddc0bca119
Emil Sadek <esadek@hotmail.com>
2021-05-18 21:36:00 -0700

    Remove: postprocessing

--- postprocess.js
+++
@@ -1,6 +0,0 @@
-import { readJSON, writeJSON } from 'https://deno.land/x/flat@0.0.10/mod.ts' 
-
-const json = await readJSON('owid-covid-data.json')
-console.log(json)
-
-await writeJSON('data.json', json.USA.data)
