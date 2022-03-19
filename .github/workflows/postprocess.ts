7c15c0316b1baca044d876b33d6f1a5e59bca96f
Emil Sadek <esadek@users.noreply.github.com>
2022-03-18 18:51:58 -0700

    update

--- .github/workflows/postprocess.ts
+++ .github/workflows/postprocess.ts
@@ -1,5 +1,4 @@
 import { format } from "https://deno.land/std@0.130.0/datetime/mod.ts";
-import { exists } from "https://deno.land/std@0.130.0/fs/exists.ts";
 import { readJSON, writeJSON } from 'https://deno.land/x/flat@0.0.15/mod.ts';
 
 const filename = Deno.args[0];
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # RudderStack Data Plane Status
+
+Monitor the status of your [RudderStack Data Plane
+
+Builth with [Flat Data](https://next.github.com/projects/flat-data/) and hosted with [GitHub Pages](https://pages.github.com/)
+
+## License
+
+[MIT](LICENSE)
