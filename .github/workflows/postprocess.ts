85061b48104526fa786df59c8669ce46482c11f2
Emil Sadek <esadek@users.noreply.github.com>
2022-03-18 18:05:26 -0700

    Update: file

--- .github/workflows/postprocess.ts
+++ .github/workflows/postprocess.ts
@@ -1,14 +1,16 @@
 import { format } from "https://deno.land/std@0.130.0/datetime/mod.ts";
-import { readJSON, writeJSON, removeFile } from 'https://deno.land/x/flat@0.0.15/mod.ts';
+import { readJSON, writeJSON } from 'https://deno.land/x/flat@0.0.15/mod.ts';
 
 
 const filename = Deno.args[0];
-console.log(filename)
+const newFilename = "postprocessed_" + filename;
 
 const data = await readJSON(filename);
-console.log(data);
+const postprocessedData = await readJSON(newFilename);
 
 data.time = format(new Date(), "yyyy-MM-dd HH:mm:ss");
 
-const newFilename = "postprocessed_" + filename;
-await writeJSON(newFilename, data);
+const newData = { postprocessedData, data };
+
+
+await writeJSON(newFilename, newData);
