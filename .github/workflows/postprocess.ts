3d0fa86c87080e820e15c89bc6ad871aa721762a
Emil Sadek <esadek@users.noreply.github.com>
2022-03-18 18:31:58 -0700

    update

--- .github/workflows/postprocess.ts
+++ .github/workflows/postprocess.ts
@@ -1,4 +1,5 @@
 import { format } from "https://deno.land/std@0.130.0/datetime/mod.ts";
+import { exists } from "https://deno.land/std@0.130.0/fs/exists.ts";
 import { readJSON, writeJSON } from 'https://deno.land/x/flat@0.0.15/mod.ts';
 
 
