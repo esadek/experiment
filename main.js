520d76479970f3cbc6c44fa1108d463f5f552860
Emil Sadek <esadek@hotmail.com>
2022-06-25 00:06:10 -0700

    Update: main.js

--- main.js
+++ main.js
@@ -1,46 +1,56 @@
-const allSystems = document.getElementById('all-systems');
-const server = document.getElementById('server');
-const database = document.getElementById('database');
-const acceptingEvents = document.getElementById('accepting-events');
-const routingEvents = document.getElementById('routing-events');
-const appType = document.getElementById('app-type');
-const mode = document.getElementById('mode');
-const goroutines = document.getElementById('goroutines');
-const backendConfigMode = document.getElementById('backend-config-mode');
-const lastSync = document.getElementById('last-sync');
+const dataPlaneUrl = document.getElementById("data-plane-url");
+const allSystems = document.getElementById("all-systems");
+const server = document.getElementById("server");
+const db = document.getElementById("database");
+const acceptingEvents = document.getElementById("accepting-events");
+const routingEvents = document.getElementById("routing-events");
 
 const checkCircle = "<i class=\"bi bi-check-circle-fill\"></i>";
-const xCircle = "<i class=\"bi bi-x-circle-fill\"></i";
-
-const upHtml = "UP " + checkCircle;
-const downHtml = "DOWN " + xCircle;
-const trueHtml = "TRUE " + checkCircle;
-const falseHtml = "FALSE " + xCircle;
+const xCircle = "<i class=\"bi bi-x-circle-fill\"></i>";
 
 const request = new XMLHttpRequest();
-
 request.open("GET", "postprocessed_data.json", true);
-
 request.onload = function () {
     const data = JSON.parse(this.response);
     const statusTimes = Object.keys(data);
     const latestStatus = data[statusTimes[statusTimes.length - 1]];
-
     const days = {};
+
     statusTimes.forEach(statusTime => {
         const day = statusTime.slice(0, 10);
-        if (!days[day]) days[day] = "UP";
-        const status = data[statusTime];
-        if (
-            status.server !== "UP" ||
-            status.db !== "UP" ||
-            status.acceptingEvents !== "TRUE" ||
-            status.routingEvents !== "TRUE"
-        ) {
-            days[day] = "DOWN";
+        if (!days[day]) {
+            days[day] = {
+                server: { up: 0, down: 0 },
+                db: { up: 0, down: 0 },
+                acceptingEvents: { up: 0, down: 0 },
+                routingEvents: { up: 0, down: 0 }
+            };
         }
+        const status = data[statusTime];
+        status.server === "UP" ? days[day].server.up += 1 : days[day].server.down +=1;
+        status.db === "UP" ? days[day].db.up += 1 : days[day].db.down +=1;
+        status.acceptingEvents === "TRUE" ? days[day].acceptingEvents.up += 1 : days[day].acceptingEvents.down +=1;
+        status.routingEvents === "TRUE" ? days[day].routingEvents.up += 1 : days[day].routingEvents.down +=1;
     });
-    console.log(days);
+
+    Object.keys(days).forEach(day => {
+        ["server", "db", "acceptingEvents", "routingEvents"].forEach(type => {
+            const up = days[day][type].up;
+            const down = days[day][type].down;
+            days[day][type] = ((up + down > 0) ? up / (up + down) : up) * 100;
+        })
+        let backgroundColorClass = "";
+        ["server", "db", "acceptingEvents", "routingEvents"].forEach(type => {
+            const uptime = days[day][type];
+            backgroundColorClass = "bg-success";
+            if (uptime < 100 && uptime > 90) {
+                backgroundColorClass = "bg-warning";
+            } else if (uptime <= 90) {
+                backgroundColorClass = "bg-danger";
+            }
+            eval(type).innerHTML += `<div class="${backgroundColorClass}" data-bs-toggle="popover" data-bs-placement="bottom" data-bs-trigger="hover focus" title="${day}" data-bs-content="${uptime.toFixed(2)}% uptime"></div>`;
+        })
+    });
 
     if (
         latestStatus.server === "UP" &&
@@ -55,43 +65,15 @@ request.onload = function () {
         allSystems.innerHTML = xCircle + "&ensp;System Down";
     }
 
-    if (latestStatus.server === "UP") {
-        server.className = "text-success";
-        server.innerHTML = upHtml;
-    } else {
-        server.className = "text-danger";
-        server.innerHTML = downHtml;
-    }
-
-    if (latestStatus.db === "UP") {
-        database.className = "text-success";
-        database.innerHTML = upHtml;
-    } else {
-        database.className = "text-danger";
-        database.innerHTML = downHtml;
-    }
-
-    if (latestStatus.acceptingEvents === 'TRUE') {
-        acceptingEvents.className = "text-success";
-        acceptingEvents.innerHTML = trueHtml;
-    } else {
-        acceptingEvents.className = "text-danger";
-        acceptingEvents.innerHTML = falseHtml;
-    }
-
-    if (latestStatus.routingEvents === 'TRUE') {
-        routingEvents.className = "text-success";
-        routingEvents.innerHTML = trueHtml;
-    } else {
-        routingEvents.className = "text-danger";
-        routingEvents.innerHTML = trueHtml;
-    }
-
-    appType.innerHTML = latestStatus.appType;
-    mode.innerHTML = latestStatus.mode;
-    goroutines.innerHTML = latestStatus.goroutines;
-    backendConfigMode.innerHTML = latestStatus.backendConfigMode;
-    lastSync.innerHTML = latestStatus.lastSync;
+    const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+    const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
 }
-
 request.send();
+
+const request2 = new XMLHttpRequest();
+request2.open("GET", ".github/workflows/flat.yml", true);
+request2.onload = function () {
+    dataPlaneUrl.innerHTML = this.response.split("\n")[22].split(": ")[1];
+    
+}
+request2.send();
